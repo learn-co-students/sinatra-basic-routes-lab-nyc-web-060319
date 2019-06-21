d25a6e47d283357d1310336e70e7bc9e27effb49
Alexander Riccio <test35965+learn@gmail.com>
2019-06-21 17:13:31 -0400

    solve lab

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
-
+require 'pry'
 class App < Sinatra::Base
+
+    get '/name' do
+        "My name is "
+    end
+
+    get '/hometown' do
+        status 200
+        "My hometown is "
+    end
+
+    get '/favorite-song' do
+        status 200
+        "My favorite song is "
+    end
 end

5c7f2330e747cd531ba4f5a7bf15e622b9448bf2
Harpreet Ghotra <harpreetghotra40@icloud.com>
2019-06-24 15:17:18 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
-require_relative 'config/environment'
+require_relative "config/environment"
 
 class App < Sinatra::Base
+  get "/name" do
+    "My name is Harpreet"
+  end
+
+  get "/hometown" do
+    "My hometown is Chandigarh"
+  end
+
+  get "/favorite-song" do
+    "My favorite song is Not Afraid"
+  end
 end
